b7af03be52e7c6e349ebf3dcb6031bbd54715de4
sean zhou <4gfaith@gmail.com>
2020-08-25 19:31:36 +0800

    琉璃

--- 词云根据csv频率.py
+++ 词云根据csv频率.py
@@ -25,13 +25,13 @@ import  matplotlib.pyplot as plt
 
 
 #设置这里就好
-font=r'.\字体\hy.ttf' #字体
-# font=r'.\字体\繁体思源宋.otf' 
-yuanfile = r'.\源文本\wjr.csv'#源文本
+font=r'./字体/hy.ttf' #字体
+# font=r'./字体/繁体思源宋.otf' 
+yuanfile = r'./源文本/琉璃.csv'#源文本
 image=np.array(Image.open('04.png')) # 图片模板
 bi_c = np.array(Image.open('02.png'))
 img_colors = ImageColorGenerator(bi_c)
-stop_words = open(".\stopwords_CN.txt",encoding="utf8").read().split("\n")
+stop_words = open("stopwords_CN.txt",encoding="utf8").read().split("\n")
 # 读取词频文件
 # fp = pd.read_csv(yuanfile, encoding='utf-8')
 fp = pd.read_csv(yuanfile)  
